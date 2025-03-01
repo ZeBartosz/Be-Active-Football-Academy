4e8e6a120d1aaf918f2731983ef99525ce09e303
Bartosz Stachowiak <stachowiak725@gmail.com>
2025-03-01 12:30:41 +0000

    chore: set up for testing

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -1,10 +1,18 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Tests;
 
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Illuminate\Support\Facades\Artisan;
 
 abstract class TestCase extends BaseTestCase
 {
-    //
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        Artisan::call('migrate');
+    }
 }
