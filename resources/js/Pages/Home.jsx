382a8e765bb386899482b44621d1f7c7605b1bda
Bartosz Stachowiak <stachowiak725@gmail.com>
2025-03-06 16:09:40 +0000

    build: add react with inertia and add tailwind to it

---
+++ resources/js/Pages/Home.jsx
@@ -0,0 +1,5 @@
+export default function Home() {
+    return<>
+    <h1 className="text-3xl font-bold underline bg-yellow-200"> Hello</h1>
+    </>
+}
--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,13 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-import tailwindcss from '@tailwindcss/vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
     plugins: [
-        laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
-        }),
-        tailwindcss(),
+        laravel(['resources/js/app.jsx', 'resources/css/app.css']),
+        react(),
+        tailwindcss()
+
     ],
 });
