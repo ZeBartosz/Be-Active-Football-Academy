f4d5810d10d9eff2080c804e06f286d6cd64ef84
Bartosz Stachowiak <112800510+ZeBartosz@users.noreply.github.com>
2025-03-14 11:46:31 +0000

    chore: add route for login and register and logout, put them in middleware groups

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,20 @@
 declare(strict_types=1);
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 
 Route::get('/', function () {
     return inertia('Home');
+});
+
+Route::middleware('guest')->group(function () {
+    Route::get('/login', [AuthController::class, 'login'])->name('login');
+    Route::post('/login', [AuthController::class, 'authenticate']);
+
+    Route::get('/register', [AuthController::class, 'register'])->name('register');
+    Route::post('/register', [AuthController::class, 'store']);
+});
+
+Route::middleware('auth')->group(function () {
+    Route::post('/logout', [AuthController::class, 'logout']);
 });
