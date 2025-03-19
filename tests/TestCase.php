4b0dec7a96c5ef768a72a62cf5b88b237a00d258
Bartosz Stachowiak <112800510+ZeBartosz@users.noreply.github.com>
2025-03-19 10:38:28 +0000

    chore: add refresh database after tests

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -4,15 +4,15 @@ declare(strict_types=1);
 
 namespace Tests;
 
+use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
-use Illuminate\Support\Facades\Artisan;
 
 abstract class TestCase extends BaseTestCase
 {
+    use RefreshDatabase; // Add this trait
+
     protected function setUp(): void
     {
         parent::setUp();
-
-        Artisan::call('migrate');
     }
 }
