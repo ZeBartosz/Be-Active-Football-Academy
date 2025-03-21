23e79efa021f5f04591e1c907fd4df81229d84ca
Bartosz Stachowiak <112800510+ZeBartosz@users.noreply.github.com>
2025-03-21 15:47:08 +0000

    chore: update policy to return 404 not found

--- app/Policies/BasePolicy.php
+++ app/Policies/BasePolicy.php
@@ -6,18 +6,23 @@ namespace App\Policies;
 
 use App\Models\User;
 use Illuminate\Auth\Access\HandlesAuthorization;
+use Illuminate\Auth\Access\Response;
 
 final class BasePolicy
 {
     use HandlesAuthorization;
 
-    public function adminAndUser(User $user, $model): bool
+    public function adminAndUser(User $user, $model): Response
     {
-        return $user->is_admin === true || $user->id === $model->user_id;
+        return $user->is_admin === true || $user->id === $model->user_id
+            ? Response::allow()
+            : Response::denyAsNotFound();
     }
 
-    public function admin(User $user): bool
+    public function admin(User $user): Response
     {
-        return $user->is_admin === true;
+        return $user->is_admin === true
+            ? Response::allow()
+            : Response::denyAsNotFound();
     }
 }
