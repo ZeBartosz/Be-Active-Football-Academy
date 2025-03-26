eb69a9e5a4ec15651c2cc87a3678cd35b97f20fd
Bartosz Stachowiak <112800510+ZeBartosz@users.noreply.github.com>
2025-03-26 12:29:29 +0000

    chore(admin): update h1 for team table

--- resources/js/Pages/Components/AdminComp/TeamTable.jsx
+++ resources/js/Pages/Components/AdminComp/TeamTable.jsx
@@ -1,7 +1,7 @@
 function TeamTable({ teams }) {
     return (
         <>
-            <h1>This is a coaches table</h1>
+            <h1>This is a Team table</h1>
             <div className="bg-opacity-75 mt-5 flex flex-col justify-center bg-[#05283d] text-white">
                 <table className="table-fixed border-collapse rounded-md border border-[#9dbebb] text-center">
                     <thead>
