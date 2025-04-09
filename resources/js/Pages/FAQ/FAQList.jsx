113c5c6deefc2077b105af3a07be0044ab6bd2b1
Bartosz Stachowiak <112800510+ZeBartosz@users.noreply.github.com>
2025-04-09 10:26:26 +0100

    feat(FAQ): add display for the questions and answers

--- resources/js/Pages/FAQ/FAQList.jsx
+++ resources/js/Pages/FAQ/FAQList.jsx
@@ -7,6 +7,14 @@ export default function FAQList({ faqs }) {
         <>
             <h1>This is the FAQ</h1>
             <Link href="/FAQ/create">Add FAQ</Link>
+            {faqs.map((faq) => (
+                <div key={faq.id}>
+                    <h2>Question:</h2>
+                    <p>{faq.question}</p>
+                    <h2>Answer:</h2>
+                    <p>{faq.answer}</p>
+                </div>
+            ))}
         </>
     );
 }
