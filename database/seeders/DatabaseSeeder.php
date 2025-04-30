4da97f14f7905df2c99773426cb9e7db27c3582d
Bartosz Stachowiak <112800510+ZeBartosz@users.noreply.github.com>
2025-04-30 10:13:53 +0100

    chore(seeder): update seeder to create more data

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -6,6 +6,8 @@ namespace Database\Seeders;
 
 use App\Models\Event;
 use App\Models\FAQ;
+use App\Models\Player;
+use App\Models\Team;
 use App\Models\User;
 use Illuminate\Database\Seeder;
 
@@ -20,14 +22,21 @@ final class DatabaseSeeder extends Seeder
     {
         User::factory(100)->create();
         Event::factory(100)->create();
-        FAQ::factory(10)->create();
+        FAQ::factory(5)->create();
+        Team::factory(5)->create();
+        Event::factory(100)->create(['team_id' => 1]);
 
-        User::factory()->create([
+        $user = User::factory()->create([
             'first_name' => 'John',
             'last_name' => 'Mart',
             'email' => 'a@a.com',
             'password' => bcrypt('123456'),
             'is_admin' => true,
+        ]);
+
+        Player::factory(5)->create([
+            'user_id' => $user->id,
+            'team_id' => 1,
         ]);
 
         User::factory()->create([
