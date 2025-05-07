c6ab0b5ee389cd11f97fd4f289f2b0204dab8d23
Bartosz Stachowiak <112800510+ZeBartosz@users.noreply.github.com>
2025-05-07 09:40:22 +0100

    feat(responsibility): create a skeleton

---
+++ app/Http/Controllers/ResponsibilityController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreResponsibilityRequest;
+use App\Http\Requests\UpdateResponsibilityRequest;
+use App\Models\Responsibility;
+
+class ResponsibilityController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreResponsibilityRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Responsibility $responsibility)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Responsibility $responsibility)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateResponsibilityRequest $request, Responsibility $responsibility)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Responsibility $responsibility)
+    {
+        //
+    }
+}
