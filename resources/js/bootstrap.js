8921b3fdfddc6e0e66d17367f3e61d11a1c9b01c
Bartosz Stachowiak <112800510+ZeBartosz@users.noreply.github.com>
2025-08-30 17:01:44 +0200

    bug: bug bugging more

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -2,22 +2,3 @@ import axios from "axios";
 window.axios = axios;
 
 window.axios.defaults.headers.common["X-Requested-With"] = "XMLHttpRequest";
-
-// Interceptor to clean up headers
-window.axios.interceptors.request.use((config) => {
-    const method = (config.method || "get").toLowerCase();
-    const isGet = method === "get";
-    const isApi = (config.url || "").startsWith("/api/");
-
-    if (isGet || !isApi) {
-        // Remove JSON headers for Inertia/page requests
-        delete config.headers["Content-Type"];
-        delete config.headers["Accept"];
-    } else {
-        // Keep JSON headers for API calls
-        config.headers["Content-Type"] = "application/json";
-        config.headers["Accept"] = "application/json";
-    }
-
-    return config;
-});
